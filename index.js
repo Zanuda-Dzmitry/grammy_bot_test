938496fd4536b0ba0f0009444c2b4dd3b114c2d3
Dzmitry_Shaviaka <krayzerkyn@gmail.com>
2025-02-10 19:45:38 +0300

    test

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 require("dotenv").config();
-const {
-  Bot,
-  GrammyError,
-  HttpError,
-  Keyboard,
-  InlineKeyboard,
-} = require("grammy");
+const { Bot } = require("grammy");
 
-// const bot = new Bot(process.env.BOT_API_KEY);
-const bot = new Bot(BOT_API_KEY);
+const bot = new Bot(process.env.BOT_API_KEY);
 
-const webhookUrl = "https://grammy-bot-test-2.vercel.app/";
+// Установка вебхука
+const webhookUrl = `https://${process.env.VERCEL_URL}/api/webhook`;
 bot.api
   .setWebhook(webhookUrl)
   .then(() => {
@@ -19,46 +13,3 @@ bot.api
   .catch((err) => {
     console.error("Error setting webhook:", err);
   });
-
-bot.api.setMyCommands([
-  { command: "start", description: "Запуск бота" },
-  { command: "info", description: "info" },
-]);
-
-bot.command("start", async (ctx) => {
-  await ctx.reply("Hello, i am Dzmitry!!!");
-});
-
-bot.hears("id", async (ctx) => {
-  await ctx.reply(`Вот ваш id ${ctx.from.id}`);
-});
-
-bot.command("inline_keyboard", async (ctx) => {
-  const keyboard = new InlineKeyboard()
-    .text("one", "one")
-    .text("twe", "twe")
-    .text("three", "three");
-
-  await ctx.reply("Выберите пункт меню", { reply_markup: keyboard });
-});
-
-bot.on("callback_query:data", async (ctx) => {
-  await ctx.answerCallbackQuery();
-  await ctx.reply(`Вы нажали кнопку: ${ctx.callbackQuery.data}`);
-});
-
-bot.catch((err) => {
-  const ctx = err.ctx;
-  console.error(`Error: ${ctx.update.update_id}:`);
-  const e = err.error;
-
-  if (e instanceof GrammyError) {
-    console.error(`GrammyError: ${e.description}`);
-  } else if (e instanceof HttpError) {
-    console.error(`HttpError: ${e}`);
-  } else {
-    console.error(`Unknown: ${e}`);
-  }
-});
-
-bot.start();
