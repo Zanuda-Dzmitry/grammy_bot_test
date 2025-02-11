70f737ca627dd197584e310361d7f23dff3e7fef
Dzmitry_Shaviaka <krayzerkyn@gmail.com>
2025-02-11 17:30:33 +0300

    test

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const { Bot } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-// Установка вебхука
-const webhookUrl = `https://${process.env.VERCEL_URL}/api/webhook`;
-bot.api
-  .setWebhook(webhookUrl)
-  .then(() => {
-    console.log("Webhook set!");
-  })
-  .catch((err) => {
-    console.error("Error setting webhook:", err);
-  });
+// Обработчик команды /start
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello, I am Dzmitry!!!");
+});
+
+bot.start();
